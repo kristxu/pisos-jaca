35901192da5410e9b782d7beede4cb24e1938189
kristxu <kristxu1983@gmail.com>
2026-06-03 08:27:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.idealista.com/alquiler-viviendas/jaca-huesca/con-precio-hasta_700/"
+
+html = requests.get(URL).text
+
+print(html[:500])
